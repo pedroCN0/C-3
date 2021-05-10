b8fb883a8e68251436ea1ecf7101453d1176bd3d
Pedro Campos Novais <62915258+pedroCN0@users.noreply.github.com>
2021-05-10 17:10:46 -0300

    Add files via upload

---
+++ 3 - Projeto DS 1308/Projeto DS 1308/Projeto DS 1308/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_DS_1308
+{
+    class Pessoa //Pessoa classe é feita por atributos e métodos
+    {
+        public string nome;
+        public string cpf;
+        public string endereço;
+        public string rg;
+        public string telefone;
+        public string email;
+
+        public void mostranom
+    }
+}
